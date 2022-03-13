564fff28a18305ec2fd20ace2c355ef27d1c9209
dimayzer <99674344+dimayzer@users.noreply.github.com>
2022-03-13 20:50:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,31 +22,32 @@ async def on_ready():
 @bot.event
 async def on_member_join (member):
 	role = discord.utils.get (member.guild.roles, id=595307817829793794) #айди роли, которая выдается каждому зашедшему
-	channel = bot.get_channel(logs_id) #канал с логами
-	emb = discord.Embed(title="Информация о пользователе", color=ctx.message.author.color)
-	emb.add_field(name="Имя:", value=ctx.message.author.display_name,inline=False)
-	emb.add_field(name="Айди пользователя:", value=ctx.message.author.id,inline=False)
-	t = ctx.message.author.status
+	channel = bot.get_channel(logs_id)
+	enter_channel_id = bot.get_channel(enter_channel) #канал с логами
+	emb = discord.Embed(title="Информация о пользователе", color=member.color)
+	emb.add_field(name="Имя:", value=member.mention,inline=False)
+	emb.add_field(name="Айди пользователя:", value=member.id,inline=False)
+	t = member.status
 	if t == discord.Status.online:
 		d = " В сети"
 
-	t = ctx.message.author.status
+	t = member.status
 	if t == discord.Status.offline:
 		d = "⚪ Не в сети"
 
-	t = ctx.message.author.status
+	t = member.status
 	if t == discord.Status.idle:
 		d = " Не активен"
 
-	t = ctx.message.author.status
+	t = member.status
 	if t == discord.Status.dnd:
 		d = " Не беспокоить"
 
 	emb.add_field(name="Активность:", value=d,inline=False)
-	emb.add_field(name="Статус:", value=ctx.message.author.activity,inline=False)
-	emb.add_field(name="Акаунт был создан:", value=ctx.message.author.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"),inline=False)
-	emb.set_thumbnail(url=ctx.message.author.avatar_url)
-	await enter_channel.send(embed = emb) #отправка логов
+	emb.add_field(name="Статус:", value=member.activity,inline=False)
+	emb.add_field(name="Акаунт был создан:", value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"),inline=False)
+	emb.set_thumbnail(url=member.avatar_url)
+	await enter_channel_id.send(embed = emb) #отправка логов
 	await channel.send(f"[log] {member.mention} зашел на сервер") #отправка логов
 	print(f"[log] {member.mention} зашел на сервер")
 	await member.add_roles( role ) #выдача роли зашедшему
